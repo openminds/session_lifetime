387b186478e5ee8650c5c0e8ba6948b0acda42c3
Jan De Poorter <jan@defv.be>
2009-02-18 16:52:30 +0100

    Got rid of the cattr_accessors, added the redirect_with options.
    Also added some documentation in-code for other people

--- lib/defv/session_lifetime.rb
+++ lib/defv/session_lifetime.rb
@@ -1,15 +1,21 @@
 module Defv
   module SessionLifetime
     module ClassMethods
+      # Set the session expiring
+      #
+      # Options:
+      #   * :time - After how much time of inactivity should the session be invalidated. Default is 1 hour
+      #   * :redirect_to - Where should we redirect the user to once their session has expired
+      #   * :redirect_with - Use a function to redirect the user. Overrides redirect_to.
+      #   * :on_expiry - Takes a Proc or a lamba which gives you a callback after the session invalidation. This is useful for setting a flash message or something in the database.
       def expires_session options = {}
-        cattr_accessor :_session_expiry
-        cattr_accessor :_redirect_to
-        cattr_accessor :_on_expiry
+        options.reverse_merge!({
+          :time => 1.hour,
+          :redirect_to => '/'
+        })
         
-        self._session_expiry = options[:time] || 1.hour
-        self._redirect_to = options[:redirect_to] || '/'
-        self._on_expiry = options[:on_expiry]
-        
+        write_inheritable_attribute "@session_expiry_options", options
+    
         self.before_filter :check_session_lifetime
       end
     end
@@ -18,17 +24,28 @@ module Defv
         protected
         
         def check_session_lifetime
-          if session[:updated_at] && session[:updated_at] + self._session_expiry < Time.now
+          # Get the attribute set by expires_session
+          options = self.class.read_inheritable_attribute('@session_expiry_options')
+
+          # Session is expired
+          if session[:updated_at] && session[:updated_at] + options[:time] < Time.now
             reset_session
-            redirect_to self._redirect_to
             
-            if self._on_expiry
-              if self._on_expiry.arity == 1
-                self._on_expiry.call(self)
+            # Redirect (either with redirect_with or redirect_to)
+            if options[:redirect_with]
+              self.send(options[:redirect_with])
+            else
+              redirect_to options[:redirect_to]
+            end
+            
+            # Call the on_expiry proc if one is given
+            if options[:on_expiry]
+              if options[:on_expiry].arity == 1
+                options[:on_expiry].call(self)
               else
-                instance_eval(&self._on_expiry)
+                instance_eval(&options[:on_expiry])
               end
-            elsif self.methods.include?('on_expiry')
+            elsif self.methods.include?('on_expiry') # Legacy, call the on_expiry method if one is defined
               self.send(:on_expiry) 
             end
           else
